3b8713bade24d9e49f690da0e06a2afc2765383a
Andrey Lesnikov <ozkriff@gmail.com>
2016-10-30 11:41:33 +0300

    android: Moved winit::Window into platform::Window

--- src/api/android/mod.rs
+++ src/api/android/mod.rs
@@ -2,6 +2,8 @@
 
 extern crate android_glue;
 
+use libc;
+
 use CreationError::{self, OsError};
 
 use winit;
@@ -21,6 +23,7 @@ mod ffi;
 
 pub struct Window {
     context: EglContext,
+    winit_window: winit::Window,
 }
 
 #[derive(Clone, Default)]
@@ -35,8 +38,9 @@ impl Window {
         pf_reqs: &PixelFormatRequirements,
         opengl: &GlAttributes<&Window>,
         _: &PlatformSpecificWindowBuilderAttributes,
-        _: &winit::Window,
+        winit_builder: winit::WindowBuilder,
     ) -> Result<Window, CreationError> {
+        let winit_window = winit_builder.build().unwrap();
         let opengl = opengl.clone().map_sharing(|w| &w.context);
         let native_window = unsafe { android_glue::get_native_window() };
         if native_window.is_null() {
@@ -50,7 +54,91 @@ impl Window {
         ).and_then(|p| p.finish(native_window as *const _)));
         Ok(Window {
             context: context,
+            winit_window: winit_window,
         })
+    }
+
+    pub fn set_title(&self, title: &str) {
+        self.winit_window.set_title(title)
+    }
+
+    pub fn show(&self) {
+        self.winit_window.show()
+    }
+
+    pub fn hide(&self) {
+        self.winit_window.hide()
+    }
+
+    pub fn get_position(&self) -> Option<(i32, i32)> {
+        self.winit_window.get_position()
+    }
+
+    pub fn set_position(&self, x: i32, y: i32) {
+        self.winit_window.set_position(x, y)
+    }
+
+    pub fn get_inner_size(&self) -> Option<(u32, u32)> {
+        self.winit_window.get_inner_size()
+    }
+
+    pub fn get_inner_size_points(&self) -> Option<(u32, u32)> {
+        self.winit_window.get_inner_size()
+    }
+
+    pub fn get_inner_size_pixels(&self) -> Option<(u32, u32)> {
+        self.winit_window.get_inner_size().map(|(x, y)| {
+            let hidpi = self.hidpi_factor();
+            ((x as f32 * hidpi) as u32, (y as f32 * hidpi) as u32)
+        })
+    }
+
+    pub fn get_outer_size(&self) -> Option<(u32, u32)> {
+        self.winit_window.get_outer_size()
+    }
+
+    pub fn set_inner_size(&self, x: u32, y: u32) {
+        self.winit_window.set_inner_size(x, y)
+    }
+
+    pub fn poll_events(&self) -> winit::PollEventsIterator {
+        self.winit_window.poll_events()
+    }
+
+    pub fn wait_events(&self) -> winit::WaitEventsIterator {
+        self.winit_window.wait_events()
+    }
+
+    pub unsafe fn platform_display(&self) -> *mut libc::c_void {
+        self.winit_window.platform_display()
+    }
+
+    pub unsafe fn platform_window(&self) -> *mut libc::c_void {
+        self.winit_window.platform_window()
+    }
+
+    pub fn create_window_proxy(&self) -> winit::WindowProxy {
+        self.winit_window.create_window_proxy()
+    }
+
+    pub fn set_window_resize_callback(&mut self, callback: Option<fn(u32, u32)>) {
+        self.winit_window.set_window_resize_callback(callback);
+    }
+
+    pub fn set_cursor(&self, cursor: winit::MouseCursor) {
+        self.winit_window.set_cursor(cursor);
+    }
+
+    pub fn hidpi_factor(&self) -> f32 {
+        self.winit_window.hidpi_factor()
+    }
+
+    pub fn set_cursor_position(&self, x: i32, y: i32) -> Result<(), ()> {
+        self.winit_window.set_cursor_position(x, y)
+    }
+
+    pub fn set_cursor_state(&self, state: winit::CursorState) -> Result<(), String> {
+        self.winit_window.set_cursor_state(state)
     }
 }
 
