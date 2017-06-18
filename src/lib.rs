cfeb78f837e03a6f0341e51de1c994eb7077620b
mitchmindtree <mitchell.nordine@gmail.com>
2017-06-18 20:16:12 +1000

    Slight cleanup in lib.rs. Remove commented code.

--- src/lib.rs
+++ src/lib.rs
@@ -57,10 +57,9 @@ extern crate cocoa;
 extern crate core_foundation;
 #[cfg(target_os = "macos")]
 extern crate core_graphics;
-#[cfg(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"))]
+#[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "dragonfly", target_os = "openbsd"))]
 extern crate x11_dl;
 #[cfg(any(target_os = "linux", target_os = "freebsd", target_os = "dragonfly", target_os = "openbsd"))]
-//#[macro_use(wayland_env)]
 extern crate wayland_client;
 
 pub use headless::{HeadlessRendererBuilder, HeadlessContext};
