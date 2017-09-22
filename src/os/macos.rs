a5aaa7aab397514d83c0df54a5d241d539d686a0
Pierre Krieger <pierre.krieger1708@gmail.com>
2017-09-22 18:16:33 +0200

    Fix reexports in os module

--- src/os/macos.rs
+++ src/os/macos.rs
@@ -1,3 +1,6 @@
 #![cfg(target_os = "macos")]
 
-pub use winit::os::macos::{ActivationPolicy, WindowBuilderExt, WindowExt};
+pub use winit::os::macos::ActivationPolicy;
+pub use winit::os::macos::MonitorIdExt;
+pub use winit::os::macos::WindowBuilderExt;
+pub use winit::os::macos::WindowExt;
--- src/os/unix.rs
+++ src/os/unix.rs
@@ -1,3 +1,7 @@
 #![cfg(any(target_os = "linux", target_os = "dragonfly", target_os = "freebsd", target_os = "openbsd"))]
 
-pub use winit::os::unix::{get_x11_xconnection, x11, WindowBuilderExt, WindowExt};
+pub use winit::os::unix::XNotSupported;
+pub use winit::os::unix::EventsLoopExt;
+pub use winit::os::unix::MonitorIdExt;
+pub use winit::os::unix::WindowBuilderExt;
+pub use winit::os::unix::WindowExt;
