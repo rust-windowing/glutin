ab74c4ba0fd1993370fd79a8db3ec775aa255c4f
Hal Gentz <zegentzy@protonmail.com>
2018-07-04 04:01:17 -0600

    Fixes event pool cloning
    Signed-off-by: Hal Gentz <zegentzy@protonmail.com>

--- src/lib.rs
+++ src/lib.rs
@@ -427,6 +427,7 @@ impl Context {
     /// Error should be very rare and only occur in case of permission denied,
     /// incompatible system, out of memory, etc.
     pub fn new(
+        el: &winit::EventsLoop,
         context_builder: ContextBuilder,
         events_loop: &EventsLoop,
         windowed: bool
@@ -434,7 +435,7 @@ impl Context {
     {
         let ContextBuilder { pf_reqs, gl_attr } = context_builder;
         let gl_attr = gl_attr.map_sharing(|ctxt| &ctxt.context);
-        platform::Context::new_context(events_loop, &pf_reqs, &gl_attr, windowed)
+        platform::Context::new_context(el, &pf_reqs, &gl_attr, windowed)
             .map(|context| Context {
                 context: context,
             })
