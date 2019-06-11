4a5f089834f1a507cb4d148f44170fe69ddc7b5a
Blake Hawkins <blake.hawkins.11@gmail.com>
2019-06-11 20:28:26 +0100

    t fix osx panic

--- glutin_examples/examples/window.rs
+++ glutin_examples/examples/window.rs
@@ -5,20 +5,21 @@ use glutin::event_loop::{ControlFlow, EventLoop};
 use glutin::window::WindowBuilder;
 use glutin::ContextBuilder;
 
-fn main() {
+fn main() -> Result<(), glutin::CreationError> {
     let el = EventLoop::new();
     let wb = WindowBuilder::new().with_title("A fantastic window!");
 
     let windowed_context =
-        ContextBuilder::new().build_windowed(wb, &el).unwrap();
+        ContextBuilder::new().build_windowed(wb, &el)?;
 
-    let windowed_context = unsafe { windowed_context.make_current().unwrap() };
+    let windowed_context = unsafe {
+       windowed_context.make_current().expect("Make current fail")
+    };
 
     println!(
         "Pixel format of the window's GL context: {:?}",
         windowed_context.get_pixel_format()
     );
-
     let gl = support::load(&windowed_context.context());
 
     el.run(move |event, _, control_flow| {
@@ -36,9 +37,10 @@ fn main() {
                 }
                 WindowEvent::RedrawRequested => {
                     gl.draw_frame([1.0, 0.5, 0.7, 1.0]);
-                    windowed_context.swap_buffers().unwrap();
+                    windowed_context.swap_buffers().expect("Swapbuffer fail");
                 }
                 WindowEvent::CloseRequested => {
+                    println!("Close requested.");
                     *control_flow = ControlFlow::Exit
                 }
                 _ => (),
