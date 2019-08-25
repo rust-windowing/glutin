6240b3ac374a7289a0e4a9909feb8f4a14989ab3
Kenny Levinsen <kl@kl.wtf>
2019-08-25 22:21:15 +0200

    Cache eglGetProcAddress

--- glutin/src/api/egl/mod.rs
+++ glutin/src/api/egl/mod.rs
@@ -14,12 +14,29 @@ mod egl {
     use super::ffi;
     use crate::api::dlloader::{SymTrait, SymWrapper};
     use libloading;
+    use std::sync::{Arc, Mutex};
+
+    #[cfg(unix)]
+    use libloading::os::unix as libloading_os;
+    #[cfg(windows)]
+    use libloading::os::windows as libloading_os;
 
     #[derive(Clone)]
     pub struct Egl(pub SymWrapper<ffi::egl::Egl>);
 
     /// Because `*const raw::c_void` doesn't implement `Sync`.
     unsafe impl Sync for Egl {}
+
+    type EglGetProcAddressType = libloading_os::Symbol<
+        unsafe extern "C" fn(
+            *const std::os::raw::c_void,
+        ) -> *const std::os::raw::c_void,
+    >;
+
+    lazy_static! {
+        static ref EGL_GET_PROC_ADDRESS: Arc<Mutex<Option<EglGetProcAddressType>>> =
+            Arc::new(Mutex::new(None));
+    }
 
     impl SymTrait for ffi::egl::Egl {
         fn load_with(lib: &libloading::Library) -> Self {
@@ -37,27 +54,32 @@ mod egl {
                     Err(_) => (),
                 };
 
-                // The symbol was not available in the library, so ask
-                // eglGetProcAddress for it. Note that eglGetProcAddress was
-                // only able to look up extension functions prior to EGL 1.5,
-                // hence this two-part dance.
-                match unsafe { lib.get(b"eglGetProcAddress") } {
-                    Ok(sym) => {
+                let mut egl_get_proc_address =
+                    (*EGL_GET_PROC_ADDRESS).lock().unwrap();
+                if egl_get_proc_address.is_none() {
+                    unsafe {
                         let sym: libloading::Symbol<
                             unsafe extern "C" fn(
                                 *const std::os::raw::c_void,
                             )
                                 -> *const std::os::raw::c_void,
-                        > = sym;
-                        unsafe {
-                            sym(std::ffi::CString::new(s.as_bytes())
-                                .unwrap()
-                                .as_bytes_with_nul()
-                                .as_ptr()
-                                as *const std::os::raw::c_void)
-                        }
+                        > = lib.get(b"eglGetProcAddress").unwrap();
+                        *egl_get_proc_address = Some(sym.into_raw());
                     }
-                    Err(_) => std::ptr::null(),
+                }
+
+                // The symbol was not available in the library, so ask
+                // eglGetProcAddress for it. Note that eglGetProcAddress was
+                // only able to look up extension functions prior to EGL 1.5,
+                // hence this two-part dance.
+                unsafe {
+                    (egl_get_proc_address.as_ref().unwrap())(
+                        std::ffi::CString::new(s.as_bytes())
+                            .unwrap()
+                            .as_bytes_with_nul()
+                            .as_ptr()
+                            as *const std::os::raw::c_void,
+                    )
                 }
             };
 
