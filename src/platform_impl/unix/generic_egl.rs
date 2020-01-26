21a1006aa41c521c8a0c3d8fe17bd4b8007599b2
Freya Gentz <zegentzy@protonmail.com>
2020-01-26 01:32:51 -0700

    Prepare for gbm support
    Signed-off-by: Freya Gentz <zegentzy@protonmail.com>

---
+++ src/platform_impl/unix/generic_egl.rs
@@ -0,0 +1,326 @@
+use crate::api::egl;
+use crate::config::{ConfigAttribs, ConfigWrapper, ConfigsFinder, SwapInterval};
+use crate::context::ContextBuilderWrapper;
+use crate::platform::unix::BackingApi;
+use crate::surface::{PBuffer, Pixmap, SurfaceTypeTrait, Window};
+use crate::utils::{NoCmp, NoPrint};
+
+use glutin_interface::{
+    NativeDisplay, NativePixmap, NativePixmapSource, NativeWindow, NativeWindowSource, RawWindow,
+    Seal, WaylandWindowParts, RawDisplay,
+};
+use wayland_client::egl as wegl;
+pub use wayland_client::sys::client::wl_display;
+use winit_types::dpi;
+use winit_types::error::{Error, ErrorType};
+
+use std::os::raw;
+use std::ops::Deref;
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Backend {
+    Wayland,
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Config {
+    Wayland(egl::Config),
+}
+
+impl Deref for Config {
+    type Target = egl::Config;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Config::Wayland(conf) => conf,
+        }
+    }
+}
+
+impl Config {
+    #[inline]
+    pub fn new<ND: NativeDisplay>(
+        cf: &ConfigsFinder,
+        nd: &ND,
+    ) -> Result<Vec<(ConfigAttribs, Config)>, Error> {
+        let glx_not_supported_error = make_error!(ErrorType::NotSupported(
+            "GLX not supported by generic_egl".to_string(),
+        ));
+        let backing_api = cf.plat_attr.backing_api;
+        match backing_api {
+            BackingApi::Glx => return Err(glx_not_supported_error),
+            BackingApi::GlxThenEgl => {
+                warn!("[glutin] Not trying GLX with generic_egl, as not supported by generic_egl.")
+            }
+            _ => (),
+        }
+
+        let configs = egl::Config::new(cf, nd.clone(), |confs, _| {
+            confs.into_iter().map(|config| Ok(config)).collect()
+        })
+        .map_err(|mut err| match backing_api {
+            BackingApi::GlxThenEgl => {
+                err.append(glx_not_supported_error);
+                err
+            }
+            _ => err,
+        })?;
+        Ok(configs
+            .into_iter()
+            .map(|(attribs, config)| (attribs, match nd.raw_display() {
+                RawDisplay::Wayland { .. } => Config::Wayland(config),
+                _ => unreachable!(),
+            }))
+            .collect())
+    }
+
+    #[inline]
+    fn backend(&self) -> Backend {
+        match self {
+            Config::Wayland(_) => Backend::Wayland,
+        }
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub(crate) enum Surface<T: SurfaceTypeTrait> {
+    WaylandWindow {
+        wsurface: NoCmp<NoPrint<wegl::WlEglSurface>>,
+        surface: egl::Surface<T>,
+    },
+    WaylandPbuffer(egl::Surface<T>),
+}
+
+impl<T: SurfaceTypeTrait> Deref for Surface<T> {
+    type Target = egl::Surface<T>;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Surface::WaylandWindow { surface, .. } | Surface::WaylandPbuffer(surface) => surface,
+        }
+    }
+}
+
+impl<T: SurfaceTypeTrait> Surface<T> {
+    #[inline]
+    pub fn is_current(&self) -> bool {
+        (**self).is_current()
+    }
+
+    #[inline]
+    pub fn get_config(&self) -> ConfigWrapper<Config, ConfigAttribs> {
+        (**self).get_config().map_config(|conf| match self.backend() {
+            Backend::Wayland => Config::Wayland(conf),
+        })
+    }
+
+    #[inline]
+    pub unsafe fn make_not_current(&self) -> Result<(), Error> {
+        (**self).make_not_current()
+    }
+
+    #[inline]
+    fn backend(&self) -> Backend {
+        match self {
+            Surface::WaylandWindow { .. } | Surface::WaylandPbuffer(_) => Backend::Wayland,
+        }
+    }
+}
+
+impl Surface<Window> {
+    #[inline]
+    pub unsafe fn new<NWS: NativeWindowSource>(
+        conf: ConfigWrapper<&Config, &ConfigAttribs>,
+        nws: &NWS,
+        wb: NWS::WindowBuilder,
+    ) -> Result<(NWS::Window, Self), Error> {
+        #[allow(deprecated)]
+        let nw = nws.build_wayland(
+            wb,
+            WaylandWindowParts {
+                _non_exhaustive_do_not_use: Seal,
+            },
+        )?;
+        Self::new_existing(conf, &nw).map(|surf| (nw, surf))
+    }
+
+    #[inline]
+    pub unsafe fn new_existing<NW: NativeWindow>(
+        conf: ConfigWrapper<&Config, &ConfigAttribs>,
+        nw: &NW,
+    ) -> Result<Self, Error> {
+        let (width, height): (u32, u32) = nw.size().into();
+
+        let surface = nw.raw_window();
+        match surface {
+            RawWindow::Wayland { wl_surface, .. } => {
+                let wl_surface =
+                    wegl::WlEglSurface::new_from_raw(wl_surface as *mut _, width as i32, height as i32);
+
+                egl::Surface::<Window>::new(conf.map_config(|conf| &**conf), wl_surface.ptr() as *const _)
+                    .map(|surface| Surface::WaylandWindow {
+                        wsurface: NoCmp(NoPrint(wl_surface)),
+                        surface,
+                    })
+            }
+            _ => unreachable!(),
+        }
+    }
+
+    #[inline]
+    pub fn update_after_resize(&self, size: &dpi::PhysicalSize<u32>) {
+        match self {
+            Surface::WaylandWindow { wsurface, .. } => {
+                let (width, height): (u32, u32) = (*size).into();
+                wsurface
+                    .resize(width as i32, height as i32, 0, 0)
+            }
+            _ => (),
+        }
+    }
+
+    #[inline]
+    pub fn swap_buffers(&self) -> Result<(), Error> {
+        (**self).swap_buffers()
+    }
+
+    #[inline]
+    pub fn swap_buffers_with_damage(&self, rects: &[dpi::Rect]) -> Result<(), Error> {
+        (**self).swap_buffers_with_damage(rects)
+    }
+
+    #[inline]
+    pub fn modify_swap_interval(&self, swap_interval: SwapInterval) -> Result<(), Error> {
+        (**self).modify_swap_interval(swap_interval)
+    }
+}
+
+impl Surface<PBuffer> {
+    #[inline]
+    pub unsafe fn new(
+        conf: ConfigWrapper<&Config, &ConfigAttribs>,
+        size: &dpi::PhysicalSize<u32>,
+    ) -> Result<Self, Error> {
+        let backend = conf.config.backend();
+        egl::Surface::<PBuffer>::new(conf.map_config(|conf| &**conf), size)
+            .map(|surf| match backend {
+            Backend::Wayland => Surface::WaylandPbuffer(surf),
+        })
+    }
+}
+
+impl Surface<Pixmap> {
+    #[inline]
+    pub unsafe fn new_existing<NP: NativePixmap>(
+        _conf: ConfigWrapper<&Config, &ConfigAttribs>,
+        _np: &NP,
+    ) -> Result<Self, Error> {
+        return Err(make_error!(ErrorType::NotSupported(
+            "generic_egl does not support pixmaps.".to_string(),
+        )));
+    }
+
+    #[inline]
+    pub unsafe fn new<NPS: NativePixmapSource>(
+        _conf: ConfigWrapper<&Config, &ConfigAttribs>,
+        _nps: &NPS,
+        _pb: NPS::PixmapBuilder,
+    ) -> Result<(NPS::Pixmap, Self), Error> {
+        return Err(make_error!(ErrorType::NotSupported(
+            "generic_egl does not support pixmaps.".to_string(),
+        )));
+    }
+}
+
+#[derive(Debug, PartialEq, Eq)]
+pub enum Context {
+    Wayland(egl::Context),
+}
+
+impl Deref for Context {
+    type Target = egl::Context;
+
+    fn deref(&self) -> &Self::Target {
+        match self {
+            Context::Wayland(ctx) => ctx,
+        }
+    }
+}
+
+impl Context {
+    #[inline]
+    pub(crate) fn new(
+        cb: ContextBuilderWrapper<&Context>,
+        conf: ConfigWrapper<&Config, &ConfigAttribs>,
+    ) -> Result<Self, Error> {
+        let backend = conf.config.backend();
+        egl::Context::new(
+            cb.map_sharing(|ctx| &**ctx),
+            conf.map_config(|conf| &**conf),
+        )
+            .map(|ctx| match backend {
+            Backend::Wayland => Context::Wayland(ctx),
+        })
+    }
+
+    #[inline]
+    pub unsafe fn make_current_surfaceless(&self) -> Result<(), Error> {
+        (**self).make_current_surfaceless()
+    }
+
+    #[inline]
+    pub(crate) unsafe fn make_current<T: SurfaceTypeTrait>(&self, surf: &Surface<T>) -> Result<(), Error> {
+        if self.backend() != surf.backend() {
+            return Err(make_error!(ErrorType::BadApiUsage(
+                "Incompatible context and surface backends.".to_string()
+            )));
+        }
+
+        (**self).make_current(&**surf)
+    }
+
+    #[inline]
+    pub(crate) unsafe fn make_current_rw<TR: SurfaceTypeTrait, TW: SurfaceTypeTrait>(
+        &self,
+        read_surf: &Surface<TR>,
+        write_surf: &Surface<TW>,
+    ) -> Result<(), Error> {
+        if self.backend() != read_surf.backend() || read_surf.backend() != write_surf.backend() {
+            return Err(make_error!(ErrorType::BadApiUsage(
+                "Incompatible context and surface backends.".to_string()
+            )));
+        }
+
+        (**self)
+            .make_current_rw(&**read_surf, &**write_surf)
+    }
+
+    #[inline]
+    pub unsafe fn make_not_current(&self) -> Result<(), Error> {
+        (**self).make_not_current()
+    }
+
+    #[inline]
+    pub fn is_current(&self) -> bool {
+        (**self).is_current()
+    }
+
+    #[inline]
+    pub fn get_proc_address(&self, addr: &str) -> Result<*const raw::c_void, Error> {
+        (**self).get_proc_address(addr)
+    }
+
+    #[inline]
+    pub fn get_config(&self) -> ConfigWrapper<Config, ConfigAttribs> {
+        (**self).get_config().map_config(|conf| match self.backend() {
+            Backend::Wayland => Config::Wayland(conf),
+        })
+    }
+
+    #[inline]
+    fn backend(&self) -> Backend {
+        match self {
+            Context::Wayland(_) => Backend::Wayland,
+        }
+    }
+}
--- src/platform_impl/unix/unix.rs
+++ src/platform_impl/unix/unix.rs
@@ -6,7 +6,7 @@
     target_os = "openbsd",
 ))]
 
-mod wayland;
+mod generic_egl;
 pub mod x11;
 
 use crate::config::{ConfigAttribs, ConfigWrapper, ConfigsFinder, SwapInterval};
@@ -25,7 +25,7 @@ use std::os::raw;
 #[derive(Debug, PartialEq, Eq)]
 pub enum Config {
     X11(x11::Config),
-    Wayland(wayland::Config),
+    GenericEgl(generic_egl::Config),
 }
 
 impl Config {
@@ -36,10 +36,10 @@ impl Config {
     ) -> Result<Vec<(ConfigAttribs, Config)>, Error> {
         Ok(match nd.raw_display() {
             RawDisplay::Wayland { .. } => {
-                let configs = wayland::Config::new(cf, nd)?;
+                let configs = generic_egl::Config::new(cf, nd)?;
                 configs
                     .into_iter()
-                    .map(|(attribs, config)| (attribs, Config::Wayland(config)))
+                    .map(|(attribs, config)| (attribs, Config::GenericEgl(config)))
                     .collect()
             }
             RawDisplay::Xlib { .. } => {
@@ -49,7 +49,7 @@ impl Config {
                     .map(|(attribs, config)| (attribs, Config::X11(config)))
                     .collect()
             }
-            // FIXME: GBM/EGLExtDevice/EGLMesaSurfaceles backends.
+            // FIXME: GBM/EGLExtDevice/EGLMesaSurfaceless backends.
             _ => unimplemented!(),
         })
     }
@@ -58,25 +58,25 @@ impl Config {
 #[derive(Debug, PartialEq, Eq)]
 pub enum Context {
     X11(x11::Context),
-    Wayland(wayland::Context),
+    GenericEgl(generic_egl::Context),
 }
 
 impl Context {
     #[inline]
-    fn inner_cb_wayland(
+    fn inner_cb_generic_egl(
         cb: ContextBuilderWrapper<&Context>,
-    ) -> Result<ContextBuilderWrapper<&wayland::Context>, Error> {
+    ) -> Result<ContextBuilderWrapper<&generic_egl::Context>, Error> {
         match cb.sharing {
-            Some(Context::Wayland(_)) | None => (),
+            Some(Context::GenericEgl(_)) | None => (),
             _ => {
                 return Err(make_error!(ErrorType::BadApiUsage(
-                    "Cannot share a Wayland context with a non-Wayland context".to_string()
+                    "Cannot share a GenericEgl context with a non-GenericEgl context".to_string()
                 )))
             }
         }
 
         Ok(cb.map_sharing(|ctx| match ctx {
-            Context::Wayland(ctx) => ctx,
+            Context::GenericEgl(ctx) => ctx,
             _ => unreachable!(),
         }))
     }
@@ -106,9 +106,9 @@ impl Context {
         conf: ConfigWrapper<&Config, &ConfigAttribs>,
     ) -> Result<Self, Error> {
         match conf.config {
-            Config::Wayland(config) => {
-                wayland::Context::new(Context::inner_cb_wayland(cb)?, conf.map_config(|_| config))
-                    .map(Context::Wayland)
+            Config::GenericEgl(config) => {
+                generic_egl::Context::new(Context::inner_cb_generic_egl(cb)?, conf.map_config(|_| config))
+                    .map(Context::GenericEgl)
             }
             Config::X11(config) => {
                 x11::Context::new(Context::inner_cb_x11(cb)?, conf.map_config(|_| config))
@@ -120,15 +120,15 @@ impl Context {
     #[inline]
     pub unsafe fn make_current_surfaceless(&self) -> Result<(), Error> {
         match self {
-            Context::Wayland(ref ctx) => ctx.make_current_surfaceless(),
+            Context::GenericEgl(ref ctx) => ctx.make_current_surfaceless(),
             Context::X11(ref ctx) => ctx.make_current_surfaceless(),
         }
     }
 
     #[inline]
-    pub unsafe fn make_current<T: SurfaceTypeTrait>(&self, surf: &Surface<T>) -> Result<(), Error> {
+    pub(crate) unsafe fn make_current<T: SurfaceTypeTrait>(&self, surf: &Surface<T>) -> Result<(), Error> {
         match (self, surf) {
-            (Context::Wayland(ref ctx), Surface::Wayland(ref surf)) => ctx.make_current(surf),
+            (Context::GenericEgl(ref ctx), Surface::GenericEgl(ref surf)) => ctx.make_current(surf),
             (Context::X11(ref ctx), Surface::X11(ref surf)) => ctx.make_current(surf),
             (_, _) => Err(make_error!(ErrorType::BadApiUsage(
                 "Incompatible context and surface backends.".to_string()
@@ -137,16 +137,16 @@ impl Context {
     }
 
     #[inline]
-    pub unsafe fn make_current_rw<TR: SurfaceTypeTrait, TW: SurfaceTypeTrait>(
+    pub(crate) unsafe fn make_current_rw<TR: SurfaceTypeTrait, TW: SurfaceTypeTrait>(
         &self,
         read_surf: &Surface<TR>,
         write_surf: &Surface<TW>,
     ) -> Result<(), Error> {
         match (self, read_surf, write_surf) {
             (
-                Context::Wayland(ref ctx),
-                Surface::Wayland(ref read_surf),
-                Surface::Wayland(ref write_surf),
+                Context::GenericEgl(ref ctx),
+                Surface::GenericEgl(ref read_surf),
+                Surface::GenericEgl(ref write_surf),
             ) => ctx.make_current_rw(read_surf, write_surf),
             (Context::X11(ref ctx), Surface::X11(ref read_surf), Surface::X11(ref write_surf)) => {
                 ctx.make_current_rw(read_surf, write_surf)
@@ -160,7 +160,7 @@ impl Context {
     #[inline]
     pub unsafe fn make_not_current(&self) -> Result<(), Error> {
         match self {
-            Context::Wayland(ref ctx) => ctx.make_not_current(),
+            Context::GenericEgl(ref ctx) => ctx.make_not_current(),
             Context::X11(ref ctx) => ctx.make_not_current(),
         }
     }
@@ -168,7 +168,7 @@ impl Context {
     #[inline]
     pub fn is_current(&self) -> bool {
         match self {
-            Context::Wayland(ref ctx) => ctx.is_current(),
+            Context::GenericEgl(ref ctx) => ctx.is_current(),
             Context::X11(ref ctx) => ctx.is_current(),
         }
     }
@@ -176,7 +176,7 @@ impl Context {
     #[inline]
     pub fn get_proc_address(&self, addr: &str) -> Result<*const raw::c_void, Error> {
         match self {
-            Context::Wayland(ref ctx) => ctx.get_proc_address(addr),
+            Context::GenericEgl(ref ctx) => ctx.get_proc_address(addr),
             Context::X11(ref ctx) => ctx.get_proc_address(addr),
         }
     }
@@ -184,23 +184,23 @@ impl Context {
     #[inline]
     pub fn get_config(&self) -> ConfigWrapper<Config, ConfigAttribs> {
         match self {
-            Context::Wayland(ref ctx) => ctx.get_config().map_config(Config::Wayland),
+            Context::GenericEgl(ref ctx) => ctx.get_config().map_config(Config::GenericEgl),
             Context::X11(ref ctx) => ctx.get_config().map_config(Config::X11),
         }
     }
 }
 
 #[derive(Debug, PartialEq, Eq)]
-pub enum Surface<T: SurfaceTypeTrait> {
+pub(crate) enum Surface<T: SurfaceTypeTrait> {
     X11(x11::Surface<T>),
-    Wayland(wayland::Surface<T>),
+    GenericEgl(generic_egl::Surface<T>),
 }
 
 impl<T: SurfaceTypeTrait> Surface<T> {
     #[inline]
     pub fn is_current(&self) -> bool {
         match self {
-            Surface::Wayland(ref surf) => surf.is_current(),
+            Surface::GenericEgl(ref surf) => surf.is_current(),
             Surface::X11(ref surf) => surf.is_current(),
         }
     }
@@ -208,7 +208,7 @@ impl<T: SurfaceTypeTrait> Surface<T> {
     #[inline]
     pub fn get_config(&self) -> ConfigWrapper<Config, ConfigAttribs> {
         match self {
-            Surface::Wayland(ref surf) => surf.get_config().map_config(Config::Wayland),
+            Surface::GenericEgl(ref surf) => surf.get_config().map_config(Config::GenericEgl),
             Surface::X11(ref surf) => surf.get_config().map_config(Config::X11),
         }
     }
@@ -216,7 +216,7 @@ impl<T: SurfaceTypeTrait> Surface<T> {
     #[inline]
     pub unsafe fn make_not_current(&self) -> Result<(), Error> {
         match self {
-            Surface::Wayland(ref surf) => surf.make_not_current(),
+            Surface::GenericEgl(ref surf) => surf.make_not_current(),
             Surface::X11(ref surf) => surf.make_not_current(),
         }
     }
@@ -229,9 +229,9 @@ impl Surface<PBuffer> {
         size: &dpi::PhysicalSize<u32>,
     ) -> Result<Self, Error> {
         match conf.config {
-            Config::Wayland(config) => {
-                wayland::Surface::<PBuffer>::new(conf.map_config(|_| config), size)
-                    .map(Surface::Wayland)
+            Config::GenericEgl(config) => {
+                generic_egl::Surface::<PBuffer>::new(conf.map_config(|_| config), size)
+                    .map(Surface::GenericEgl)
             }
             Config::X11(config) => {
                 x11::Surface::<PBuffer>::new(conf.map_config(|_| config), size).map(Surface::X11)
@@ -248,9 +248,9 @@ impl Surface<Pixmap> {
         pb: NPS::PixmapBuilder,
     ) -> Result<(NPS::Pixmap, Self), Error> {
         match conf.config {
-            Config::Wayland(config) => {
-                wayland::Surface::<Pixmap>::new(conf.map_config(|_| config), nps, pb)
-                    .map(|(pix, surf)| (pix, Surface::Wayland(surf)))
+            Config::GenericEgl(config) => {
+                generic_egl::Surface::<Pixmap>::new(conf.map_config(|_| config), nps, pb)
+                    .map(|(pix, surf)| (pix, Surface::GenericEgl(surf)))
             }
             Config::X11(config) => {
                 x11::Surface::<Pixmap>::new(conf.map_config(|_| config), nps, pb)
@@ -265,9 +265,9 @@ impl Surface<Pixmap> {
         np: &NP,
     ) -> Result<Self, Error> {
         match conf.config {
-            Config::Wayland(config) => {
-                wayland::Surface::<Pixmap>::new_existing(conf.map_config(|_| config), np)
-                    .map(Surface::Wayland)
+            Config::GenericEgl(config) => {
+                generic_egl::Surface::<Pixmap>::new_existing(conf.map_config(|_| config), np)
+                    .map(Surface::GenericEgl)
             }
             Config::X11(config) => {
                 x11::Surface::<Pixmap>::new_existing(conf.map_config(|_| config), np)
@@ -285,9 +285,9 @@ impl Surface<Window> {
         wb: NWS::WindowBuilder,
     ) -> Result<(NWS::Window, Self), Error> {
         match conf.config {
-            Config::Wayland(config) => {
-                wayland::Surface::<Window>::new(conf.map_config(|_| config), nws, wb)
-                    .map(|(win, surf)| (win, Surface::Wayland(surf)))
+            Config::GenericEgl(config) => {
+                generic_egl::Surface::<Window>::new(conf.map_config(|_| config), nws, wb)
+                    .map(|(win, surf)| (win, Surface::GenericEgl(surf)))
             }
             Config::X11(config) => {
                 x11::Surface::<Window>::new(conf.map_config(|_| config), nws, wb)
@@ -302,9 +302,9 @@ impl Surface<Window> {
         nw: &NW,
     ) -> Result<Self, Error> {
         match conf.config {
-            Config::Wayland(config) => {
-                wayland::Surface::<Window>::new_existing(conf.map_config(|_| config), nw)
-                    .map(Surface::Wayland)
+            Config::GenericEgl(config) => {
+                generic_egl::Surface::<Window>::new_existing(conf.map_config(|_| config), nw)
+                    .map(Surface::GenericEgl)
             }
             Config::X11(config) => {
                 x11::Surface::<Window>::new_existing(conf.map_config(|_| config), nw)
@@ -316,7 +316,7 @@ impl Surface<Window> {
     #[inline]
     pub fn swap_buffers(&self) -> Result<(), Error> {
         match self {
-            Surface::Wayland(ref surf) => surf.swap_buffers(),
+            Surface::GenericEgl(ref surf) => surf.swap_buffers(),
             Surface::X11(ref surf) => surf.swap_buffers(),
         }
     }
@@ -324,7 +324,7 @@ impl Surface<Window> {
     #[inline]
     pub fn swap_buffers_with_damage(&self, rects: &[dpi::Rect]) -> Result<(), Error> {
         match self {
-            Surface::Wayland(ref surf) => surf.swap_buffers_with_damage(rects),
+            Surface::GenericEgl(ref surf) => surf.swap_buffers_with_damage(rects),
             Surface::X11(ref surf) => surf.swap_buffers_with_damage(rects),
         }
     }
@@ -332,7 +332,7 @@ impl Surface<Window> {
     #[inline]
     pub fn update_after_resize(&self, size: &dpi::PhysicalSize<u32>) {
         match self {
-            Surface::Wayland(ref surf) => surf.update_after_resize(size),
+            Surface::GenericEgl(ref surf) => surf.update_after_resize(size),
             Surface::X11(_) => (),
         }
     }
@@ -340,7 +340,7 @@ impl Surface<Window> {
     #[inline]
     pub fn modify_swap_interval(&self, swap_interval: SwapInterval) -> Result<(), Error> {
         match self {
-            Surface::Wayland(ref surf) => surf.modify_swap_interval(swap_interval),
+            Surface::GenericEgl(ref surf) => surf.modify_swap_interval(swap_interval),
             Surface::X11(ref surf) => surf.modify_swap_interval(swap_interval),
         }
     }
