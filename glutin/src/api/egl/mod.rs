45ea59817ecf03cb15fdf44a5bbcbafb475faea1
Richard <richardsoncusto@gmail.com>
2021-10-26 11:15:47 -0300

    Implement sRGB support for EGL

--- glutin/src/api/egl/mod.rs
+++ glutin/src/api/egl/mod.rs
@@ -816,8 +816,20 @@ impl<'a> ContextPrototype<'a> {
     pub fn finish(self, nwin: ffi::EGLNativeWindowType) -> Result<Context, CreationError> {
         let egl = EGL.as_ref().unwrap();
         let surface = unsafe {
+            let colorspace = if self.pixel_format.srgb {
+                ffi::egl::GL_COLORSPACE_SRGB
+            } else {
+                ffi::egl::GL_COLORSPACE_LINEAR
+            };
+
+            let attrs = &[
+                ffi::egl::GL_COLORSPACE as raw::c_int,
+                colorspace as raw::c_int,
+                ffi::egl::NONE as raw::c_int,
+            ];
+
             let surface =
-                egl.CreateWindowSurface(self.display, self.config_id, nwin, std::ptr::null());
+                egl.CreateWindowSurface(self.display, self.config_id, nwin, attrs.as_ptr());
             if surface.is_null() {
                 return Err(CreationError::OsError("eglCreateWindowSurface failed".to_string()));
             }
@@ -1126,8 +1138,6 @@ where
             out.push(xid as raw::c_int);
         }
 
-        // FIXME: srgb is not taken into account
-
         match pf_reqs.release_behavior {
             ReleaseBehavior::Flush => (),
             ReleaseBehavior::None => {
@@ -1238,7 +1248,7 @@ where
             0 | 1 => None,
             a => Some(a as u16),
         },
-        srgb: false, // TODO: use EGL_KHR_gl_colorspace to know that
+        srgb: pf_reqs.srgb,
     };
 
     Ok((config_id, desc))
--- glutin_egl_sys/build.rs
+++ glutin_egl_sys/build.rs
@@ -38,6 +38,7 @@ fn main() {
                 "EGL_EXT_platform_wayland",
                 "EGL_EXT_platform_device",
                 "EGL_KHR_swap_buffers_with_damage",
+                "EGL_KHR_gl_colorspace",
             ],
         );
 
